cd33e2a4d00da868d0fb262d2c9d3e31e84e42da
as468579 <as468579@gmail.com>
2019-09-21 20:24:25 +0800

    Initial Commit

---
+++ 00657023_ioshw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00657023_ioshw1
+//
+//  Created by User05 on 2019/9/21.
+//  Copyright © 2019 Yi-Chien Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
